e801e61c32bc1d135607f6e95559a0b2dfa08b78
zhangwei <zhangwei@ainirobot.com>
2019-07-19 14:41:36 +0800

    init repo

---
+++ Core/Inc/sensor_ultrasonic.h
@@ -0,0 +1,76 @@
+#ifndef __SENSOR_ULTRASONIC__
+#define __SENSOR_ULTRASONIC__
+#include <main.h>
+#include <stdbool.h>
+typedef enum ultra_type
+{
+    ultra_type_hc_sr04,
+    ultra_type_nums,
+} ultra_type_t;
+#define get_ultra_type_str(id) ({ \
+    char *name; \
+    switch (id) { \
+    case ultra_type_hc_sr04: \
+        name = "hc_sr04"; \
+        break; \
+    default:name = "unkown"; \
+    } \
+    name; \
+    })
+
+typedef enum
+{
+    ultra_id_1,
+    //    ultra_id_2,
+    //    ultra_id_3,
+    ultra_max_nums,
+} ultra_id_t;
+typedef struct hc_sr04_irq_desc{
+    ultra_id_t ultra_id; //we can get struct ultrasonic according to ultra id.
+    uint8_t irq_num; //
+    bool has_bound;
+} hc_sr04_irq_desc_t;
+typedef enum ultrasonic_ranging_state_machine
+{
+    HC_SR04_STATE_BEGIN,
+    HC_SR04_STATE_IDLE,
+    HC_SR04_STATE_SEND_ULTRASOUND,
+    HC_SR04_STATE_RAISE_ECHO_START,
+    HC_SR04_STATE_RAISE_ECHO_END,
+    HC_SR04_STATE_RAISE_CALC_DIS,
+    HC_SR04_STATE_END,
+    ULTRA_STATE_UNKNOWN,
+} ultrasonic_ranging_state_machine_t;
+typedef struct hc_sr04_resource
+{
+    GPIO_TypeDef *trigger_gpio_port;
+    uint16_t trigger_gpio_pin;
+    GPIO_TypeDef *echo_gpio_port;
+    uint16_t echo_gpio_pin;
+    struct hc_sr04_irq_desc irq_desc;
+} hc_sr04_resource_t;
+
+typedef struct ultrasonic {
+    ultra_id_t id;
+    enum ultra_type type;
+    uint16_t timeout;
+    uint32_t echo_pulse; //unit:us
+    volatile ultrasonic_ranging_state_machine_t ranging_state;
+    const char *hardware_name;
+    uint16_t max_ranging_freq;	//unit:HZ
+    void *platform_data;
+    int8_t (*init)(struct ultrasonic *);
+    int16_t (*get_distance)(struct ultrasonic *);
+    uint16_t (*get_ranging_freq)(struct ultrasonic *);
+    ultrasonic_ranging_state_machine_t (*get_ranging_state)(struct ultrasonic *);
+    int8_t (*set_ranging_state)(struct ultrasonic *, ultrasonic_ranging_state_machine_t);
+} ultrasonic_t;
+
+typedef int8_t (*ultrasonic_init_t)(struct ultrasonic *);
+typedef int16_t (*ultrasonic_get_distance_t)(struct ultrasonic *);
+typedef uint16_t (*ultrasonic_get_ranging_freq_t)(struct ultrasonic *);
+typedef ultrasonic_ranging_state_machine_t (*get_ranging_state_t)(struct ultrasonic *);
+typedef int8_t (*set_ranging_state_t)(struct ultrasonic *, ultrasonic_ranging_state_machine_t);
+extern int8_t ultrasonic_setup(ultra_type_t type, uint16_t ultra_dev_nums);
+extern struct ultrasonic *ultrasonic_get_by_id(uint16_t id);
+#endif
---
+++ Core/Src/sensor_ultrasonic.c
@@ -0,0 +1,287 @@
+#include <sensor_ultrasonic.h>
+#include <stdio.h>
+//workaround
+#define pr_err printf
+#define sr04_delay(time)
+#define HC_SR04_MAX_RANGING_FREQ (40U)
+#define VELOCITY (340.0f) //velocity:340m/s
+//hc_sr04_ranging_state_machine_t hc_sr04_ranging_state_machine;
+hc_sr04_resource_t hc_sr04_res[ultra_max_nums] = {
+    [ultra_id_1] = {
+        .trigger_gpio_port = sr04_01_trigger_pin_GPIO_Port,
+        .trigger_gpio_pin = sr04_01_trigger_pin_Pin,
+        .echo_gpio_port = sr04_01_echo_pin_GPIO_Port,
+        .echo_gpio_pin = sr04_01_echo_pin_Pin,
+    },
+
+};
+struct ultrasonic ultrasonic_table[ultra_max_nums] = {
+    [ultra_id_1] = {
+    },
+};
+#if 0
+#undef offsetof
+#define offsetof(TYPE, MEMBER) ((size_t) & ((TYPE *)0)->MEMBER)
+#define container_of(ptr, type, member) ({             \
+    const typeof(((type *)0)->member) *__mptr = (ptr); \
+    (type *)((char *)__mptr - offsetof(type, member)); \
+})
+#endif
+#define STM32F4_EXTI_NUMS (16)
+#define gpio_pin_to_exti_line(pin) ({ \
+    uint16_t pin_tmp = pin;           \
+    uint16_t exti_line_num = 0;       \
+    do                                \
+    {                                 \
+        pin_tmp = pin_tmp >> 1;       \
+        exti_line_num++;              \
+    } while (pin_tmp);                \
+    --exti_line_num;                  \
+})
+struct hc_sr04_irq_desc *hc_sr04_irq_bind_table[STM32F4_EXTI_NUMS];
+static int8_t hc_sr04_bind_irq(struct ultrasonic *ultra)
+{
+    uint16_t exti_line_num = 0;
+    struct ultrasonic *pultra = ultra;
+    int8_t ret = 0;
+    hc_sr04_resource_t *res = ultra->platform_data;
+    if (!res) {
+        pr_err("%s[%d]: args invalid!\n", __func__, __LINE__);
+        return -1;
+    }
+    exti_line_num = gpio_pin_to_exti_line(res->echo_gpio_pin);
+    if (0 <= exti_line_num && exti_line_num < STM32F4_EXTI_NUMS) {
+        res->irq_desc.irq_num = exti_line_num;
+        res->irq_desc.ultra_id = pultra->id;
+        res->irq_desc.has_bound = true;
+        hc_sr04_irq_bind_table[exti_line_num] = &res->irq_desc;
+        printf("%s[%d]:hc-sr04[%d] has bound to irq[%d]\n", __func__, __LINE__, res->irq_desc.ultra_id, res->irq_desc.irq_num);
+    } else {
+        pr_err("%s[%d]:exti_line_num is out of range\n", __func__, __LINE__);
+        res->irq_desc.has_bound = false;
+        ret = -1;
+    }
+    return ret;
+}
+static inline void hc_sr04_trigger_process(hc_sr04_resource_t *res)
+{
+    HAL_GPIO_WritePin(res->trigger_gpio_port, res->trigger_gpio_pin, 1);
+    sr04_delay(10);
+    HAL_GPIO_WritePin(res->trigger_gpio_port, res->trigger_gpio_pin, 0);
+}
+static inline struct ultrasonic *hc_sr04_get_dev_by_pin(uint16_t GPIO_Pin)
+{
+    uint16_t exti_line_num = 0;
+    struct hc_sr04_irq_desc *irq_desc = NULL;
+    struct ultrasonic *pultra = NULL;
+    //get ultra id according to exti line.
+    exti_line_num = gpio_pin_to_exti_line(GPIO_Pin);
+    if (0 <= exti_line_num && exti_line_num < STM32F4_EXTI_NUMS)
+    {
+        irq_desc = hc_sr04_irq_bind_table[exti_line_num];
+        if (irq_desc->has_bound) {
+            pultra = ultrasonic_get_by_id(irq_desc->ultra_id);
+        } else {
+            //pr_err("%s[%d]:exti_line_num is out of range\n", __func__, __LINE__);
+        }
+    }
+    return pultra;
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+    struct ultrasonic *pultra = NULL;
+    GPIO_PinState pin_state;
+    hc_sr04_resource_t *res = NULL;
+    ultrasonic_ranging_state_machine_t state = ULTRA_STATE_UNKNOWN;
+    pultra = hc_sr04_get_dev_by_pin(GPIO_Pin);
+    if (pultra) {
+        state = pultra->get_ranging_state(pultra);
+        res = (hc_sr04_resource_t *)pultra->platform_data;
+        pin_state = HAL_GPIO_ReadPin(res->echo_gpio_port, res->echo_gpio_pin);
+        switch (state) {
+        case HC_SR04_STATE_RAISE_ECHO_START:
+            //raise echo start with a rising edge
+            if (GPIO_PIN_SET == pin_state) {
+                //clear echo pulse
+                pultra->echo_pulse = 0;
+                //start timer
+                //set ranging state
+                pultra->set_ranging_state(pultra, HC_SR04_STATE_RAISE_ECHO_END);
+            }
+            break;
+        case HC_SR04_STATE_RAISE_ECHO_END:
+            //raise echo start with a falling edge
+            if (GPIO_PIN_RESET == pin_state) {
+                //stop timer
+                //set echo pulse
+                pultra->echo_pulse = 1;
+                //set ranging state
+                pultra->set_ranging_state(pultra, HC_SR04_STATE_RAISE_CALC_DIS);
+            }
+            break;
+        default:;
+        }
+    }
+}
+
+int8_t hc_sr04_init(struct ultrasonic *ultra)
+{
+    hc_sr04_resource_t *res;
+    if (!ultra || !(ultra->platform_data)) {
+        pr_err("%s[%d]: args invalid!\n", __func__, __LINE__);
+        return -1;
+    }
+    res = (struct hc_sr04_resource *)ultra->platform_data;
+    //init gpio
+    HAL_GPIO_WritePin(res->trigger_gpio_port, res->trigger_gpio_pin, 0);
+    HAL_GPIO_WritePin(res->echo_gpio_port, res->echo_gpio_pin, 0);
+    //init timer
+    //enbale echo pin irq line
+    hc_sr04_bind_irq(ultra);
+    //init state
+    ultra->set_ranging_state(ultra, HC_SR04_STATE_IDLE);
+    return 0;
+}
+int16_t hc_sr04_get_distance(struct ultrasonic *ultra)
+{
+    int16_t distance = -1; //uint:cm
+    float range, fly_time;
+    ultrasonic_ranging_state_machine_t state = ULTRA_STATE_UNKNOWN;
+    switch (state) {
+    case HC_SR04_STATE_IDLE:
+        hc_sr04_trigger_process(ultra->platform_data);
+        ultra->set_ranging_state(ultra, HC_SR04_STATE_RAISE_ECHO_START);
+        //todo: timeout check.
+        break;
+    case HC_SR04_STATE_SEND_ULTRASOUND:
+        //do nothing
+        break;
+    case HC_SR04_STATE_RAISE_CALC_DIS:
+        //caculate real distance
+        fly_time = (float)ultra->echo_pulse / 1000000.0f;
+        range = fly_time * VELOCITY / 2;
+        printf("range:%f\n", range);
+        //Start a new round of measurement
+        ultra->set_ranging_state(ultra, HC_SR04_STATE_IDLE);
+        break;
+    default:;
+    }
+    return distance;
+}
+uint16_t hc_sr04_get_ranging_freq(struct ultrasonic *ultra)
+{
+    uint16_t freq = 0;
+    freq = HC_SR04_MAX_RANGING_FREQ;
+    ultra->max_ranging_freq = freq;
+    return freq;
+}
+
+
+
+int8_t ultrasonic_install_hook(struct ultrasonic *ultra,
+                               ultrasonic_init_t init,
+                               ultrasonic_get_distance_t get_distance,
+                               ultrasonic_get_ranging_freq_t get_ranging_freq)
+{
+    if (!ultra || !get_distance) {
+        pr_err("%s[%d]: args invalid!\n", __func__, __LINE__);
+        return -1;
+    }
+    if (init)
+        ultra->init = init;
+    if (get_distance)
+        ultra->get_distance = get_distance;
+    if (get_ranging_freq)
+        ultra->get_ranging_freq = get_ranging_freq;
+    return 0;
+}
+int8_t ultrasonic_set_id(struct ultrasonic *ultra, ultra_id_t id,const char *name)
+{
+    int8_t ret = 0;
+    if (!ultra) {
+        pr_err("%s[%d]: args invalid!\n", __func__, __LINE__);
+        return -1;
+    }
+    if (id >= 0 && id < ultra_max_nums)
+        ultra->id = id;
+    else {
+        pr_err("%s[%d]: id<%d> is invalid!\n", __func__, __LINE__, id);
+        ret = -1;
+    }
+    if (name)
+        ultra->hardware_name = name;
+    else
+        ultra->hardware_name = NULL;
+    return ret;
+}
+struct ultrasonic *ultrasonic_get_by_id(uint16_t id)
+{
+    struct ultrasonic *ultra = NULL;
+    if (id < 0 || id >= ultra_max_nums) {
+        pr_err("%s[%d]: ultra id<%u> is out of range\n", __func__, __LINE__, id);
+        return NULL;
+    }
+    if ((ultrasonic_table[id].id == id)
+        && ultrasonic_table[id].platform_data 
+        && ultrasonic_table[id].get_distance) {
+        pr_err("%s[%d]: Get ultra-%d successful!!\n", __func__, __LINE__, id);
+        ultra = &ultrasonic_table[id];
+    } else
+        pr_err("%s[%d]: ultra-%d is invalid,please setup first!!\n", __func__, __LINE__, id);
+    return ultra;
+}
+int8_t ultrasonic_set_platdata(struct ultrasonic *ultra, void* data)
+{
+    int8_t ret = 0;
+    if (!ultra || !data)
+    {
+        pr_err("%s[%d]: args invalid!\n", __func__, __LINE__);
+        return -1;
+    }
+    ultra->platform_data = data;
+    return ret;
+}
+ultrasonic_ranging_state_machine_t ultra_get_ranging_state(struct ultrasonic *ultra)
+{
+    return ultra->ranging_state;
+}
+int8_t ultra_set_ranging_state(struct ultrasonic *ultra, ultrasonic_ranging_state_machine_t state)
+{
+    ultra->ranging_state = state;
+    return 0; 
+}
+
+int8_t ultrasonic_config_state_machine(struct ultrasonic *ultra, set_ranging_state_t set_func, get_ranging_state_t get_func)
+{
+    int8_t ret = 0;
+    if (!ultra)
+    {
+        pr_err("%s[%d]: args invalid!\n", __func__, __LINE__);
+        return -1;
+    }
+    ultra->set_ranging_state = (!set_func) ? ultra_set_ranging_state :
+        set_func;
+        ultra->get_ranging_state = (!get_func) ? ultra_get_ranging_state : get_func;
+        return ret;
+}
+
+int8_t ultrasonic_setup(ultra_type_t type, uint16_t ultra_dev_nums)
+{
+    uint16_t ultra_nums;
+    if (type < 0 || type >= ultra_type_nums) {
+        pr_err("%s[%d]: ultra type<%u> is unknown\n", __func__, __LINE__, type);
+        return -1;
+    }
+    //TODO:get how many ultrasonic we have in meissa board.
+    ultra_nums = ultra_dev_nums;
+    printf("%s:we have %d %s ultrasonic\n", __func__, ultra_nums, get_ultra_type_str(type));
+    //Set the corresponding callback function according to different sensor types
+    for (int i = 0; i < ultra_nums; i++) {
+        ultrasonic_set_id(&ultrasonic_table[i], i, get_ultra_type_str(type));
+        ultrasonic_config_state_machine(&ultrasonic_table[i], NULL, NULL);
+        ultrasonic_install_hook(&ultrasonic_table[i], hc_sr04_init, hc_sr04_get_distance, hc_sr04_get_ranging_freq);
+        ultrasonic_set_platdata(&ultrasonic_table[i], &hc_sr04_res[i]);
+    }
+    return 0;
+}
